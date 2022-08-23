44bbdf804af3232fb8683306d51d7a9f1b849dbb
lucastoian <lucastoian@outlook.com>
2022-08-23 15:24:14 +0200

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -46,7 +46,7 @@ function filename (file) {
    // const extension = FILE_TYPE_MAP[file.mimetype];
    let fileName = file.originalname.split(' ').join('-');
    fileName = fileName.split('-')[0];
-   fileName = fileName.replace('+', 'a');
+   fileName = fileName.replaceAll('+', 'a');
    const extension = FILE_TYPE_MAP[file.mimetype];
     //return randomWords({ exactly: 5, join: '' }) +"." + extension;
     return  `${fileName}.${extension}`;
