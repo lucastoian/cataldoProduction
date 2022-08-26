40ff68108df3c1d85d86e18b321fa1af9d09893a
lucastoian <lucastoian@outlook.com>
2022-08-26 13:24:46 +0200

    fix

--- dist/libs/users/lib/register-component/register-component.component.d.ts
+++ dist/libs/users/lib/register-component/register-component.component.d.ts
@@ -4,21 +4,24 @@ import { Router } from '@angular/router';
 import { User } from '../models/user';
 import { AuthService } from '../services/auth.service';
 import { LocalstorageService } from '../services/localstorage.service';
+import { Location } from '@angular/common';
 import * as i0 from "@angular/core";
 export declare class RegisterComponentComponent implements OnInit {
     private formBuilder;
     private authService;
     private localstorageService;
     private router;
+    private location;
     loginFormGroup: FormGroup;
     isSubmitted: boolean;
     authError: boolean;
     authMessage: string;
     user: User;
-    constructor(formBuilder: FormBuilder, authService: AuthService, localstorageService: LocalstorageService, router: Router);
+    constructor(formBuilder: FormBuilder, authService: AuthService, localstorageService: LocalstorageService, router: Router, location: Location);
     ngOnInit(): void;
     private _initLoginForm;
     onSubmit(): void;
+    goToLogin(): void;
     get loginForm(): {
         [key: string]: import("@angular/forms").AbstractControl;
     };
