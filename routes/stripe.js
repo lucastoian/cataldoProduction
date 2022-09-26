bf82a92bb112bfd1f28005dd5965eb6a5e56b8f2
Luca Stoian <luca.stoian.ext@siemens.com>
2022-09-26 20:33:08 +0200

    bad

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyparser = require('body-parser')
 const app = express();
+const Stripe = require('stripe');
 app.use(bodyparser.urlencoded({ extended: false }))
 app.use(bodyparser.json())
 const stripe = require("stripe")("sk_test_51LYUCGDEyiX3e3PlOF9sfkE3RE3eyjDbq9kzY39MZmErzUDUKIfURmQ7raSXznjlT1gQP6hUI8VBraRlzZzp6EW700HHZP3bpO");
