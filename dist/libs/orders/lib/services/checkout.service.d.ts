6361e7cd60c626d474fd4990a3dd3343d9fc1ddc
Luca Stoian <luca.stoian.ext@siemens.com>
2022-10-16 17:46:09 +0200

    go

--- dist/libs/orders/lib/services/checkout.service.d.ts
+++ dist/libs/orders/lib/services/checkout.service.d.ts
@@ -3,10 +3,12 @@ import { Observable } from 'rxjs';
 import * as i0 from "@angular/core";
 export declare class CheckoutService {
     private http;
+    private apiURLOrders;
+    private apiURLProducts;
+    private apiUrlW_Products;
     private environment;
     constructor(http: HttpClient, environment: any);
     makePayment(stripeToken: any, amount: number): Observable<any>;
-    klarnaPayment(sourceId: any, amount: number): Observable<any>;
     sendSms(orderId: any, clientNumber: any, clientPrefix: any): Observable<any>;
     parseJwt(token: any): any;
     static ɵfac: i0.ɵɵFactoryDeclaration<CheckoutService, never>;
