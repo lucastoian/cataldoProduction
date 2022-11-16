7b9389798d35183222e9bddbd66bd7b0ad3e1f9a
Luca Stoian <luca.stoian.ext@siemens.com>
2022-11-16 12:43:55 +0100

    go

--- dist/libs/products/lib/pages/products-list/products-list.component.d.ts
+++ dist/libs/products/lib/pages/products-list/products-list.component.d.ts
@@ -1,10 +1,28 @@
 import { OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Paginator } from 'primeng/paginator';
 import { Category } from '../../models/category';
 import { Product123 } from '../../models/product123';
+import { W_Product } from '../../models/w-product';
 import { CategoriesService } from '../../services/categories.service';
 import { ProductsService } from '../../services/products.service';
 import * as i0 from "@angular/core";
+interface Sesso {
+    name: string;
+    code: string;
+}
+interface Categoria {
+    name: string;
+    code: string;
+}
+interface Brand {
+    name: string;
+    code: string;
+}
+interface Taglia {
+    name: string;
+    code: string;
+}
 export declare class ProductsListComponent implements OnInit {
     private productsService;
     private catService;
@@ -19,12 +37,32 @@ export declare class ProductsListComponent implements OnInit {
     categoryId: any;
     selectedCategories: any[];
     sex: string;
+    currentId: string;
+    brandName: string;
+    product: Product123;
+    paginator: Paginator;
+    productOnThisPage: Product123[];
+    wProds: W_Product[];
+    filteredProducts: Product123[];
+    currentPage: any;
+    fromCarousel: boolean;
+    first: number;
+    sesso: Sesso[];
+    selectedSex: Sesso[];
+    categoria: Categoria[];
+    selectedCategoria: Categoria[];
+    brand: Brand[];
+    selectedBrand: Brand[];
+    taglia: Taglia[];
+    selectedTaglia: Taglia[];
     constructor(productsService: ProductsService, catService: CategoriesService, route: ActivatedRoute);
     ngOnInit(): void;
+    changePage(event: any): void;
+    search(event: any): void;
     private _getProducts;
-    private _getCategories;
     categoryFilter(): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<ProductsListComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<ProductsListComponent, "eshop-frontend-products-list", never, {}, {}, never, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<ProductsListComponent, "eshop-frontend-products-list", never, { "product": "product"; }, {}, never, never>;
 }
+export {};
 //# sourceMappingURL=products-list.component.d.ts.map
