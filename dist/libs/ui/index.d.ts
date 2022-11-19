70c01f7e1bd960a08cf80f0156bf9041c5bec506
lucastoian <72082971+lucastoian@users.noreply.github.com>
2022-11-19 14:26:29 +0000

    budapest

--- dist/libs/ui/index.d.ts
+++ dist/libs/ui/index.d.ts
@@ -1,6 +1,7 @@
 export * from './lib/ui';
 export * from './lib/ui.module';
 export * from './lib/components/gallery/gallery.component';
+export * from './lib/components/carousel/carousel.component';
 export * from './lib/banner/banner.component';
 export * from './lib/slider/slider.component';
 //# sourceMappingURL=index.d.ts.map
--- dist/libs/ui/lib/ui.module.d.ts
+++ dist/libs/ui/lib/ui.module.d.ts
@@ -2,12 +2,13 @@ import * as i0 from "@angular/core";
 import * as i1 from "./banner/banner.component";
 import * as i2 from "./slider/slider.component";
 import * as i3 from "./components/gallery/gallery.component";
-import * as i4 from "@angular/common";
-import * as i5 from "primeng/button";
-import * as i6 from "primeng/carousel";
+import * as i4 from "./components/carousel/carousel.component";
+import * as i5 from "@angular/common";
+import * as i6 from "primeng/button";
+import * as i7 from "primeng/carousel";
 export declare class UiModule {
     static ɵfac: i0.ɵɵFactoryDeclaration<UiModule, never>;
-    static ɵmod: i0.ɵɵNgModuleDeclaration<UiModule, [typeof i1.BannerComponent, typeof i2.SliderComponent, typeof i3.GalleryComponent], [typeof i4.CommonModule, typeof i5.ButtonModule, typeof i6.CarouselModule], [typeof i1.BannerComponent, typeof i2.SliderComponent, typeof i3.GalleryComponent]>;
+    static ɵmod: i0.ɵɵNgModuleDeclaration<UiModule, [typeof i1.BannerComponent, typeof i2.SliderComponent, typeof i3.GalleryComponent, typeof i4.CarouselComponent], [typeof i5.CommonModule, typeof i6.ButtonModule, typeof i7.CarouselModule], [typeof i1.BannerComponent, typeof i2.SliderComponent, typeof i3.GalleryComponent, typeof i4.CarouselComponent]>;
     static ɵinj: i0.ɵɵInjectorDeclaration<UiModule>;
 }
 //# sourceMappingURL=ui.module.d.ts.map
