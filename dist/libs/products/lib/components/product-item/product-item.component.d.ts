5a5d71f3cd14b1e2bb7bb435b199467175653396
Luca Stoian <luca.stoian.ext@siemens.com>
2022-12-03 17:13:40 +0100

    go

--- dist/libs/products/lib/components/product-item/product-item.component.d.ts
+++ dist/libs/products/lib/components/product-item/product-item.component.d.ts
@@ -1,12 +1,11 @@
 import { OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { Product123 } from '../../models/product123';
 import { ProductsService } from '../../services/products.service';
 import * as i0 from "@angular/core";
 export declare class ProductItemComponent implements OnInit {
     private route;
     private prodService;
-    private router;
     w: boolean;
     url: any;
     imgBlob: any;
@@ -16,12 +15,11 @@ export declare class ProductItemComponent implements OnInit {
     varianti: any[];
     product: Product123;
     appBackImg: any;
-    constructor(route: ActivatedRoute, prodService: ProductsService, router: Router);
+    constructor(route: ActivatedRoute, prodService: ProductsService);
     ngOnInit(): Promise<void>;
     private _retrieveParam;
     ajaxGetImageData(url: any): Promise<Blob>;
     changeCardVariant(variant: any, index: any): Promise<void>;
-    navigateTo(id: any): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<ProductItemComponent, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<ProductItemComponent, "eshop-frontend-product-item", never, { "product": "product"; "appBackImg": "appBackImg"; }, {}, never, never>;
 }
