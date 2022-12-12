fb45a0e25653fc4a08f64944ae49b186bbda4606
Luca Stoian <luca.stoian.ext@siemens.com>
2022-12-12 10:57:09 +0100

    gio

--- dist/libs/orders/lib/components/cart-icon/cart-icon.component.d.ts
+++ dist/libs/orders/lib/components/cart-icon/cart-icon.component.d.ts
@@ -1,10 +1,11 @@
 import { OnInit } from '@angular/core';
-import { Cart } from '@eshop-frontend/users';
+import { CartService } from '../../services/cart.service';
 import * as i0 from "@angular/core";
 export declare class CartIconComponent implements OnInit {
+    private cartService;
     private cart;
     cartCount: number;
-    constructor(cart: Cart);
+    constructor(cartService: CartService);
     ngOnInit(): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<CartIconComponent, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<CartIconComponent, "orders-cart-icon", never, {}, {}, never, never>;
