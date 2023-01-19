a05f676b135b7401031496baa19373e226b5d228
Luca Stoian <luca.stoian.ext@siemens.com>
2023-01-19 10:37:29 +0100

    go

--- dist/libs/orders/lib/pages/cart-page/cart-page.component.d.ts
+++ dist/libs/orders/lib/pages/cart-page/cart-page.component.d.ts
@@ -1,16 +1,19 @@
 import { ProductVariant } from '@eshop-frontend/users';
 import { OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Title, Meta } from "@angular/platform-browser";
 import { CartService } from '../../services/cart.service';
 import * as i0 from "@angular/core";
 export declare class CartPageComponent implements OnInit, OnDestroy {
     private router;
     private cartService;
+    private titleService;
+    private meta;
     productVariants: ProductVariant[];
     items: number;
     productNames: string[];
     private cart;
-    constructor(router: Router, cartService: CartService);
+    constructor(router: Router, cartService: CartService, titleService: Title, meta: Meta);
     ngOnInit(): void;
     ngOnDestroy(): void;
     backToShop(): void;
